e1870fc31832d4dc2b4d5095a3346f2cd095cdc2
webanantgupta <anantguptastu0698@gmail.com>
2025-10-24 19:05:20 +0530

    solve

--- src/Pages/newsAndUpdates/News_Zealand.jsx
+++ src/Pages/newsAndUpdates/News_Zealand.jsx
@@ -3,9 +3,9 @@ import PageBreadcrumb from '../../Common/PageBreadcrumb'
 import Gridcomponent from '../../Common/Gridcomponent'
 import { useParams } from 'react-router-dom'
 
-const News_Zealand = () => {
+const News_Zealand = () => { 
   // const country = "New Zealand";
-  const {country } = useParams();
+  const {country} = useParams();
   return (
     <div>
         <PageBreadcrumb title={`Latest news about ${country}`}/>
